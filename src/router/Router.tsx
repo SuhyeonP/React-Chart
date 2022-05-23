8fa9bdf92bceea6f2d1f32be79c2222b34cd1875
suhyeonP <ahah12k@gmail.com>
2022-05-23 16:49:18 +0900

    :art: add router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import App from '~/src/App';
 import { EChart } from '~/src/component/page';
+import BaseLayout from '~/src/component/molecules/layout/BaseLayout';
 
 const Router = (): JSX.Element => {
   return (
     <>
       <Routes>
-        <Route path="/">
+        <Route path="/" element={<BaseLayout />}>
           <Route index element={<App />} />
           <Route path="/e-charts" element={<EChart />} />
         </Route>
